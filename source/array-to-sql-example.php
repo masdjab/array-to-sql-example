416483aef8b486111b092f0a6935f25c98c29229
Heryudi Praja <heryudi.praja@redawning.com>
2020-01-06 00:35:37 +0700

    Added support for null values

--- source/array-to-sql-example.php
+++ source/array-to-sql-example.php
@@ -6,15 +6,19 @@
   $contoh_data = 
     [
       ["C001", "Siti Marfu'ah", "Mrican\r\nGejayan", "Yogyakarta", 32000], 
-      ["C002", "Aris Munandar", "Kotabaru", "Yogyakarta", 125000], 
-      ["C003", "Zainal Arifin", "Umbulharjo", "Yogyakarta", 2500000]
+      ["C002", "Aris Munandar", "Kotabaru", "Yogyakarta", null], 
+      ["C003", "Zainal Arifin", null, "Yogyakarta", 2500000]
     ];
   
   $mysqli = mysqli_connect("localhost", "root", "");
   $query = "INSERT INTO customer(" . join(", ", array_map(function($x){return "`$x`";}, $kolom_data)) . ") VALUES \r\n";
   $jml_data = count($contoh_data);
   for($i = 0; $i < $jml_data; $i++) {
-    $values_arr = array_map(function($x) use($mysqli) {return '"' . mysqli_escape_string($mysqli, $x) . '"';}, $contoh_data[$i]);
+    $mapper = 
+      function($x) use($mysqli) {
+        return !is_null($x) ?  '"' . mysqli_escape_string($mysqli, $x) . '"' : "NULL";
+      };
+    $values_arr = array_map($mapper, $contoh_data[$i]);
     $values_str = join(", ", $values_arr);
     $query = $query . "(" . $values_str . ")" . ($i == ($jml_data - 1) ? ";" : ",") . "\r\n";
   }
